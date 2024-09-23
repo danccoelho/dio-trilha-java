f859078f4d4ddc7fc7598f00cea21e790bc4a0ae
Daniel Coelho <coelhodaniel06087@gmail.com>
2024-09-23 17:01:30 -0300

    criando conta no banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.println("Por favor, digite o nome do usuário: ");
+        String nome = sc.nextLine();
+        System.out.println("Por favor, digite o número da Agência: ");
+        String numAgencia = sc.nextLine();
+        System.out.println("Por favor, digite o número: ");
+        int numero = sc.nextInt();
+        System.out.println("Por favor, digite o saldo: ");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá, " + nome + ", número de Usuário: " + numero + ". Obrigado por criar uma conta em nosso banco, sua agência é " + numAgencia + " e seu saldo é " +
+        saldo + "! Já está disponível para saque.");
+
+        
     }
-}
+}  
